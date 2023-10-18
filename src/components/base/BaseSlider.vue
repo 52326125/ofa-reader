1b79c4db0b99469a21eb4c07f26d7ee35c4724a6
aaaa931 <st876504@gmail.com>
2023-10-18 16:29:04 +0800

    feat: slider

---
+++ src/components/base/BaseSlider.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/antd.css'
+</script>
+
+<template>
+  <VueSlider
+    :process-style="{ background: '#845400' }"
+    :dot-style="{
+      border: '2px solid #845400'
+    }"
+  />
+</template>
+
+<style scoped lang="sass"></style>
