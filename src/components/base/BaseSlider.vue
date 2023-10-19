1cce42082cbeb1177a654e40ee9af5fffb732611
aaaa931 <st876504@gmail.com>
2023-10-19 11:17:39 +0800

    fix: code-review

--- src/components/base/BaseSlider.vue
+++ src/components/base/BaseSlider.vue
@@ -1,15 +1,36 @@
 <script setup lang="ts">
 import VueSlider from 'vue-slider-component'
-import 'vue-slider-component/theme/antd.css'
 </script>
 
 <template>
-  <VueSlider
-    :process-style="{ background: '#845400' }"
-    :dot-style="{
-      border: '2px solid #845400'
-    }"
-  />
+  <VueSlider />
 </template>
 
-<style scoped lang="sass"></style>
+<style scoped lang="sass">
+:deep(.vue-slider-rail)
+  background: $neutral-80
+
+:deep(.vue-slider-dot-handle)
+  border-color: $primary
+
+  &:has(.vue-slider:hover)
+    border-color: $primary
+
+:deep(.vue-slider-process)
+  background: $primary
+
+.vue-slider:hover
+  &:deep(.vue-slider-dot-handle),
+  &:deep(.vue-slider-dot-handle:hover)
+    border-color: $primary-60
+
+  &:deep(.vue-slider-process)
+    background: $primary-60
+
+:deep(.vue-slider-dot-handle-focus)
+  box-shadow: 0 0 0 5px rgba($primary, .2)
+
+:deep(.vue-slider-dot-tooltip-inner)
+  background: $primary
+  border-color: $primary
+</style>
